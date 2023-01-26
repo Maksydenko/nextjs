0daaea70383e32fe6c14297e50aa1afae694a50f
Maksym Davydenko <maksydenko@gmail.com>
2023-01-26 21:00:52 +0200

    react

---
+++ src/components/ResponsiveObjectOut.js
@@ -0,0 +1,19 @@
+import "./scss/ResponsieObjectOut.scss";
+
+function ResponsieObjectOut() {
+  return (
+    <section className="section-name">
+      <div className="section-name__content">
+        <div className="section-name__container">
+          <div className="section-name__body">
+            <h1 className="section-name__title"></h1>
+            <p className="section-name__text"></p>
+          </div>
+        </div>
+      </div>
+      <div className="section-name__img">
+        <img src="" alt="" />
+      </div>
+    </section>
+  );
+}
