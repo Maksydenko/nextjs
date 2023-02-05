77d1325547f5086a56f2a906b0f208a08867ade9
Maksym Davydenko <maksydenko@gmail.com>
2023-02-05 21:55:08 +0200

    Upd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./common/Header";
+import Footer from "./common/Footer";
 
 function App() {
   return (
