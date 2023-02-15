f3ee9b688e37c7282a38310def041edb508c5b69
Maksym Davydenko <maksydenko@gmail.com>
2023-02-15 21:45:48 +0200

    upd

--- src/components/layout/navigation/Menu/Items/Items.js
+++ src/components/layout/navigation/Menu/Items/Items.js
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom";
-
 import Item from "./Item";
 
 function Items(props) {
--- src/i18next.js
+++ src/i18next.js
@@ -3,17 +3,32 @@ import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
+import translationEn from "@assets/locales/en/translation.json";
+import translationUa from "@assets/locales/ua/translation.json";
+
+const resources = {
+  en: {
+    translation: translationEn,
+  },
+  ua: {
+    translation: translationUa,
+  },
+};
+
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: "en",
-    supportedLngs: ["en", "ua"],
+    fallbackLng: {
+      "ua-UA": ["ua"],
+      default: ["en"],
+    },
     detection: {
       order: ["localStorage"],
       cache: ["localStorage"],
     },
+    resources,
   });
 
 export default i18n;
