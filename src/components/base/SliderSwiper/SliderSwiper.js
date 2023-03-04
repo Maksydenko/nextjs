f5f3c8f8aeb65026cbb04b5c836a3c9c114ed087
Maksym Davydenko <maksydenko@gmail.com>
2023-03-04 23:33:27 +0200

    upd

--- src/components/base/SliderSwiper/SliderSwiper.js
+++ src/components/base/SliderSwiper/SliderSwiper.js
@@ -1,11 +1,14 @@
 // import Swiper core and required modules
 import {
   Navigation,
-  // Pagination,
-  // Scrollbar,
-  // Keyboard,
+  Pagination,
   // HashNavigation,
+  Keyboard,
+  // Mousewheel,
+  // FreeMode,
+  // Autoplay,
   // Parallax,
+  Virtual,
 } from "swiper";
 
 // Import Swiper React components
@@ -15,111 +18,221 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/scss";
 import "swiper/scss/navigation";
 import "swiper/scss/pagination";
-// import "swiper/scss/scrollbar";
-import "swiper/scss/keyboard";
+import "swiper/scss/zoom";
 
 function SliderSwiper(props) {
+  const {
+    className,
+    children,
+    // Navigation
+    navigation,
+    // Pagination
+    pagination,
+    // Clickable
+    paginationClickable,
+    // Dynamic bullets
+    paginationDynamicBullets,
+    // Types: bullets, fraction, progressbar
+    paginationType,
+
+    // Turning/disabling dragging on a PC
+    simulateTouch,
+    // Sweep sensitivity
+    touchRatio,
+    // Sweep/dragging angle
+    touchAngle,
+    // Grab cursor
+    grabCursor,
+    // Switching when clicking on a slide
+    slideToClickedSlide,
+
+    // Hash navigation
+    hash,
+    // Track the condition
+    hashNavigationWatchState,
+
+    // Keyboard management
+
+    // Turn on/off
+    keyboardEnabled,
+    // Turn on/off only when the slider is within the viewport
+    keyboardOnlyInViewport,
+    // Turn on/off the control control of PageUp, PageDown
+    keyboardPageUpDown,
+
+    // Mouse wheel control
+    mousewheel,
+    // The sensitivity of mouse wheel
+    mousewheelSensitivity,
+
+    // Auto height
+    autoHeight,
+    // Number of slides for showing
+    slidesPerView,
+    // The indent between the slides
+    spaceBetween,
+    // The number of flipped slides
+    slidesPerGroup,
+    // Active slide in the center
+    centeredSlides,
+    // Starting slide
+    initialSlide,
+    // Loop slider
+    loop,
+    // Free mode
+    freeMode,
+
+    // Autoplay
+    autoplay,
+    // Pause between slides
+    autoplayDelay,
+    // Stop on last slide
+    autoplayStopOnLastSlide,
+    // Disable after manual override
+    autoplayDisableOnInteraction,
+
+    // Speed
+    speed,
+    // Horizontal/vertical slider
+    direction,
+    // Breakpoints (adaptive)
+    breakpoints,
+    // Update slider when slider items change
+    observer,
+    // Turn on/off parallax
+    parallax,
+    // Virtual slides
+    virtual,
+  } = props;
+
+  const slides = children.map((slide, index) => (
+    <SwiperSlide
+      key={index}
+      virtualIndex={index}
+      className={`${className}__slide`}
+      data-hash={hash && `${hash}-${index}`}
+    >
+      {slide}
+    </SwiperSlide>
+  ));
+
   return (
     <Swiper
-      className={`${props.className}__slider`}
+      className={`${className}__slider`}
       // Modules
       modules={[
         Navigation,
-        // Pagination,
-        // Scrollbar
-        // Keyboard,
+        Pagination,
         // HashNavigation,
+        Keyboard,
+        // Mousewheel,
+        // FreeMode,
+        // Autoplay,
         // Parallax,
+        Virtual,
       ]}
       // Arrows
-      navigation
+      {...(navigation && { navigation })}
       // Pagination
-      pagination={{
-        // Clickable
-        clickable: props.paginationClickable,
-        // Dynamic bullets
-        dynamicBullets: props.paginationDynamicBullets,
-        // Types: bullets, fraction, progressbar
-        type: props.paginationType,
-      }}
-      // Scroll
-      // scrollbar={{
-      //   // The ability to drag scroll
-      //   draggable: props.scrollBarDraggable,
-      // }}
+      {...(pagination && {
+        pagination: {
+          // Clickable
+          clickable: paginationClickable,
+          // Dynamic bullets
+          dynamicBullets: paginationDynamicBullets,
+          // Types: bullets, fraction, progressbar
+          type: paginationType,
+        },
+      })}
       // Turning/disabling dragging on a PC
-      simulateTouch={props.simulateTouch}
+      simulateTouch={simulateTouch}
       // Sweep sensitivity
-      touchRatio={props.touchRatio}
+      touchRatio={touchRatio}
       // Sweep/dragging angle
-      touchAngle={props.touchAngle}
+      touchAngle={touchAngle}
       // Grab cursor
-      grabCursor={props.grabCursor}
+      grabCursor={grabCursor}
       // Switching when clicking on a slide
-      slideToClickedSlide={props.slideToClickedSlide}
+      slideToClickedSlide={slideToClickedSlide}
       // Hash navigation
-      {...(props.hash && {
+      {...(hash && {
         hashNavigation:
           // Track the condition
-          { watchState: props.hashNavigationWatchState },
+          { watchState: hashNavigationWatchState },
       })}
       // Keyboard management
-      keyboard={{
-        // Turn on/off
-        enabled: props.keyboardEnabled,
-        // Turn on/off only when the slider within the voucher
-        onlyInViewport: props.keyboardOnlyInViewport,
-        // Turn on/off the control control of PageUp, PageDown
-        pageUpDown: props.keyboardPageUpDown,
-      }}
+      {...(keyboardEnabled && {
+        keyboard: {
+          // Turn on/off
+          enabled: keyboardEnabled,
+          // Turn on/off only when the slider is within the viewport
+          onlyInViewport: keyboardOnlyInViewport,
+          // Turn on/off the control control of PageUp, PageDown
+          pageUpDown: keyboardPageUpDown,
+        },
+      })}
+      // Mouse wheel control
+      {...(mousewheel && {
+        mousewheel: {
+          // The sensitivity of mouse wheel
+          sensitivity: mousewheelSensitivity,
+        },
+      })}
       // Auto height
-      autoHeight={props.autoHeight}
+      autoHeight={autoHeight}
       // Number of slides for showing
-      slidesPerView={props.slidesPerView}
+      slidesPerView={slidesPerView}
       // The indent between the slides
-      spaceBetween={props.spaceBetween}
+      spaceBetween={spaceBetween}
       // The number of flipped slides
-      slidesPerGroup={props.slidesPerGroup}
+      slidesPerGroup={slidesPerGroup}
       // Active slide in the center
-      centeredSlides={props.centeredSlides}
+      centeredSlides={centeredSlides}
       // Starting slide
-      initialSlide={props.initialSlide}
+      initialSlide={initialSlide}
       // Loop slider
-      loop={props.loop}
+      loop={loop}
+      // Free mode
+      freeMode={freeMode}
+      // Autoplay
+      {...(autoplay && {
+        autoplay: {
+          // Pause between slides
+          delay: autoplayDelay,
+          // Stop on last slide
+          stopOnLastSlide: autoplayStopOnLastSlide,
+          // Disable after manual override
+          disableOnInteraction: autoplayDisableOnInteraction,
+        },
+      })}
       // Speed
-      speed={props.speed}
+      speed={speed}
       // Horizontal/vertical slider
-      direction={props.direction}
+      direction={direction}
       // Breakpoints (adaptive)
-      breakpoints={props.breakpoints}
+      breakpoints={breakpoints}
+      // Update slider when slider items change
+      observer={observer}
       // Turn on/off parallax
-      parallax={props.parallax}
+      parallax={parallax}
+      // Virtual slides
+      virtual={virtual}
     >
-      {props.children.map((slide, index) => (
-        <SwiperSlide
-          key={index}
-          className={`${props.className}__slide`}
-          data-hash={props.hash && `${props.hash}-${index}`}
-        >
-          {slide}
-        </SwiperSlide>
-      ))}
+      {slides}
     </Swiper>
   );
 }
 
 SliderSwiper.defaultProps = {
+  navigation: true,
   // Pagination
-
+  pagination: true,
   // Clickable
   paginationClickable: true,
   // Dynamic bullets
   paginationDynamicBullets: false,
   // Types: bullets, fraction, progressbar
   paginationType: "",
-
-  // The ability to drag scroll
-  // scrollBarDraggable: true,
 
   // Turning/disabling dragging on a PC
   simulateTouch: true,
@@ -141,10 +254,15 @@ SliderSwiper.defaultProps = {
 
   // Turn on/off
   keyboardEnabled: true,
-  // Turn on/off only when the slider within the voucher
+  // Turn on/off only when the slider is within the viewport
   keyboardOnlyInViewport: true,
   // Turn on/off the control control of PageUp, PageDown
   keyboardPageUpDown: false,
+
+  // Mouse wheel control
+  mousewheel: false,
+  // The sensitivity of mouse wheel
+  mousewheelSensitivity: 1,
 
   // Auto height
   autoHeight: true,
@@ -160,14 +278,30 @@ SliderSwiper.defaultProps = {
   initialSlide: 0,
   // Loop slider
   loop: false,
+  // Free mode
+  freeMode: false,
+
+  // Autoplay
+  autoplay: false,
+  // Pause between slides
+  autoplayDelay: 1000,
+  // Stop on last slide
+  autoplayStopOnLastSlide: false,
+  // Disable after manual override
+  autoplayDisableOnInteraction: false,
+
   // Speed
   speed: 300,
-  // Horizontal/vertical slider
+  // horizontal/vertical slider
   direction: "horizontal",
   // Breakpoints (adaptive)
-  breakpoints: "",
+  breakpoints: {},
+  // Update slider when slider items change
+  observer: true,
   // Turn on/off parallax
   parallax: false,
+  // Virtual slides
+  virtual: false,
 };
 
 export default SliderSwiper;
--- src/pages/PageNotFound/PageNotFound.js
+++ src/pages/PageNotFound/PageNotFound.js
@@ -1,6 +1,23 @@
+import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 
 function PageNotFound() {
+  const [time, setTime] = useState(10);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (time <= 0) {
+      navigate("/");
+    } else {
+      const timer = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [time]);
+
   const { t } = useTranslation();
 
   return (
@@ -9,6 +26,9 @@ function PageNotFound() {
         <div className="page-not-found__container">
           <h1 className="page-not-found__title">404</h1>
           <h2 className="page-not-found__label">{t("page-not-found")}</h2>
+          <p className="page-not-found__redirect-homepage">
+            {t("redirect-homepage")} {time}
+          </p>
         </div>
       </div>
     </main>
