141b6812808f3875ece3e59bb357ab3506f57336
Maksym Davydenko <maksydenko@gmail.com>
2023-03-16 01:03:11 +0200

    upd

--- src/components/base/Img/Img.js
+++ src/components/base/Img/Img.js
@@ -1,11 +1,24 @@
+import { useState, useEffect } from "react";
+
 import ImgPicture from "./ImgPicture";
+import Loader from "@base/Loader/Loader";
 
 function Img(props) {
-  const { className, picture, img } = props;
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const imgItem = document.querySelector("[class*='__img'] img");
+    imgItem.addEventListener("load", () => {
+      setIsLoading(false);
+    });
+  }, [isLoading]);
+
+  const { className, img } = props;
 
   return (
     <div className={`${className}__img`}>
-      <ImgPicture picture={picture} img={img} />
+      {isLoading && <Loader />}
+      <ImgPicture img={img} />
     </div>
   );
 }
--- src/components/base/Img/ImgPicture.js
+++ src/components/base/Img/ImgPicture.js
@@ -1,11 +1,25 @@
 function ImgPicture(props) {
-  const { picture, img } = props;
+  const { img } = props;
 
-  if (picture) {
+  if (Array.isArray(img)) {
+    const pictures = img
+      .slice(0, -1)
+      .map((picture, index) => (
+        <source
+          key={index}
+          srcSet={picture.src}
+          type={`image/${picture.type}`}
+        />
+      ));
+
     return (
       <picture>
-        <source srcSet={picture.src} type={`image/${picture.type}`} />
-        <img src={img.src} alt={img.alt} loading="lazy" />
+        {pictures}
+        <img
+          src={img[img.length - 1].src}
+          alt={img[img.length - 1].alt}
+          loading="lazy"
+        />
       </picture>
     );
   }
