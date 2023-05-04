381cc81ed449201f4b8b7efed999a010c783f118
Maksym Davydenko <maksydenko@gmail.com>
2023-05-04 18:32:15 +0300

    upd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,11 @@
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-
 import type { AppProps } from "next/app";
-
-import { store, persistor } from "@/store/store";
-
+import { Provider } from "react-redux";
+import { store } from "@/provider/store";
 import "@/assets/scss/globals.scss";
 
 const App = ({ Component, pageProps }: AppProps) => (
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <Component {...pageProps} />
-    </PersistGate>
+    <Component {...pageProps} />
   </Provider>
 );
 
