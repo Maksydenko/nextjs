936229b3c708e0ee1a4a7633b755d6bdffa430c5
Maksym Davydenko <maksydenko@gmail.com>
2023-06-12 09:39:41 +0300

    upd

--- src/components/layout/navigation/Menu/Menu.tsx
+++ src/components/layout/navigation/Menu/Menu.tsx
@@ -7,20 +7,20 @@ import { handleClassName } from "@/utils/className.util";
 import { links } from "../links/links.const";
 
 interface MenuProps {
-  isLockedScroll: boolean;
+  isScrollLocked: boolean;
   onClick: () => void;
 }
 
-const Menu: FC<MenuProps> = ({ isLockedScroll, onClick }) => (
+const Menu: FC<MenuProps> = ({ isScrollLocked, onClick }) => (
   <div className="header__menu menu">
     <button
       type="button"
-      className={handleClassName(isLockedScroll, "menu__button")}
+      className={handleClassName(isScrollLocked, "menu__button")}
       onClick={onClick}
     >
       <span></span>
     </button>
-    <nav className={handleClassName(isLockedScroll, "menu__body")}>
+    <nav className={handleClassName(isScrollLocked, "menu__body")}>
       <ul className="menu__list">
         <Items links={links} onClick={onClick} />
       </ul>
