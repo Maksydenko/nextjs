e6604302cd9e7cbc928f2beb1e3d4ead8583250f
Maksym Davydenko <maksydenko@gmail.com>
2023-07-07 20:09:09 +0300

    upd

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -3,70 +3,7 @@ import { FC } from "react";
 interface IHome {}
 
 const Home: FC<IHome> = () => {
-  return (
-    <ol>
-      <li>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-        ullam fuga voluptatum eaque nobis deleniti possimus impedit minima
-        beatae aliquam, veniam sapiente saepe nemo ab, unde exercitationem
-        reiciendis necessitatibus libero.
-      </li>
-      <li>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-        ullam fuga voluptatum eaque nobis deleniti possimus impedit minima
-        beatae aliquam, veniam sapiente saepe nemo ab, unde exercitationem
-        reiciendis necessitatibus libero.
-      </li>
-      <li>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-        ullam fuga voluptatum eaque nobis deleniti possimus impedit minima
-        beatae aliquam, veniam sapiente saepe nemo ab, unde exercitationem
-        reiciendis necessitatibus libero.
-      </li>
-      <li>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-        ullam fuga voluptatum eaque nobis deleniti possimus impedit minima
-        beatae aliquam, veniam sapiente saepe nemo ab, unde exercitationem
-        reiciendis necessitatibus libero.
-      </li>
-      <li>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-        ullam fuga voluptatum eaque nobis deleniti possimus impedit minima
-        beatae aliquam, veniam sapiente saepe nemo ab, unde exercitationem
-        reiciendis necessitatibus libero.
-      </li>
-      <li>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-        ullam fuga voluptatum eaque nobis deleniti possimus impedit minima
-        beatae aliquam, veniam sapiente saepe nemo ab, unde exercitationem
-        reiciendis necessitatibus libero.
-      </li>
-      <li>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-        ullam fuga voluptatum eaque nobis deleniti possimus impedit minima
-        beatae aliquam, veniam sapiente saepe nemo ab, unde exercitationem
-        reiciendis necessitatibus libero.
-      </li>
-      <li>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-        ullam fuga voluptatum eaque nobis deleniti possimus impedit minima
-        beatae aliquam, veniam sapiente saepe nemo ab, unde exercitationem
-        reiciendis necessitatibus libero.
-      </li>
-      <li>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-        ullam fuga voluptatum eaque nobis deleniti possimus impedit minima
-        beatae aliquam, veniam sapiente saepe nemo ab, unde exercitationem
-        reiciendis necessitatibus libero.
-      </li>
-      <li>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-        ullam fuga voluptatum eaque nobis deleniti possimus impedit minima
-        beatae aliquam, veniam sapiente saepe nemo ab, unde exercitationem
-        reiciendis necessitatibus libero.
-      </li>
-    </ol>
-  );
+  return <></>;
 };
 
 export default Home;
