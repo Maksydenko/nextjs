882bb20fdce34f4a195384208adfbd9330798822
Maksym Davydenko <maksydenko@gmail.com>
2023-08-14 19:35:23 +0300

    upd

--- src/hooks/useBreakpointCheck.tsx
+++ src/hooks/useBreakpointCheck.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import { useWindowListener } from "./useWindowListener";
 
@@ -9,12 +9,12 @@ interface IUseBreakpointCheck {
 export const useBreakpointCheck: IUseBreakpointCheck = (breakpoint) => {
   const [isBreakpoint, setIsBreakpoint] = useState(false);
 
-  const handleBreakpointCheck = () => {
+  const handleBreakpointCheck = useCallback(() => {
     const { innerWidth } = window;
     const isLessBreakpoint = innerWidth < breakpoint;
 
     setIsBreakpoint(isLessBreakpoint);
-  };
+  }, [breakpoint]);
   useWindowListener("resize", handleBreakpointCheck);
 
   return isBreakpoint;
