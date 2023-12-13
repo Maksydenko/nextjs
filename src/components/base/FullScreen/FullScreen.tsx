c344a8915c139747d452ade4dd48ceaabc42b1c5
Maksym Davydenko <maksydenko@gmail.com>
2023-12-13 17:07:28 +0200

    FullScreen fixes

--- src/components/base/FullScreen/FullScreen.tsx
+++ src/components/base/FullScreen/FullScreen.tsx
@@ -5,17 +5,26 @@ interface FullScreenProps {
   className?: string;
   children?: ReactNode;
   background?: ReactNode;
+  color?: string;
 }
 
 const FullScreen: FC<FullScreenProps> = ({
   className,
   children,
   background,
+  color,
 }) => {
+  const colorStyle = {
+    background: color,
+  };
+
   return (
     <div className={clsx(className, "full-screen")}>
-      <div className="full-screen__body">{children}</div>
-      {background}
+      {children && <div className="full-screen__body">{children}</div>}
+      {background && (
+        <div className="full-screen__background">{background}</div>
+      )}
+      {color && <div className="full-screen__color" style={colorStyle}></div>}
     </div>
   );
 };
